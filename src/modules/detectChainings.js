68061d7a5bf2537b589585e43f02a8672d5c1752
Leo <eleventiy96@gmail.com>
2020-11-16 23:03:16 +0200

    Chaining ...

--- src/modules/detectChainings.js
+++ src/modules/detectChainings.js
@@ -1,29 +1,73 @@
 import rects from './createRects';
 
-// function detectDistance(x1, y1, x2, y2) {
-//   const a = x1 - x2;
-//   const b = y1 - y2;
-
-//   return Math.sqrt(a * a + b * b);
-// }
-
 /**
  * Chain rectangles
  */
 export default function detectChainings() {
-  const currentRect = rects.find((rect) => rect.isDragging);
+  const maxDist = 10;
+  const minDist = 0;
+  const distance = (rect1, rect2) => {
+    return Math.sqrt((rect2.x - rect1.x) ** 2 + (rect2.y - rect1.y) ** 2);
+  };
 
-  rects.forEach((rect) => {
-    if (rect === currentRect) {
-      return;
+  const currentRect = rects.find((rect) => rect.isDragging);
+  const closestRect = rects.find((rect) => {
+    const distX = parseInt(distance(currentRect, rect) - rect.width, 10);
+    const distY = parseInt(distance(currentRect, rect) - rect.height, 10);
+
+    if ((distX >= minDist && distX <= maxDist) || (distY >= minDist && distY <= maxDist)) {
+      return rect;
     }
 
-    console.log('Rect1 left', currentRect.x, 'Rect2 left', rect.x);
-    console.log(
-      'Rect1 right',
-      currentRect.x + currentRect.width,
-      'Rect2 right',
-      rect.x + rect.width
-    );
+    return null;
   });
+
+  if (closestRect) {
+    /* const distX = parseInt(distance(currentRect, closestRect) - closestRect.width, 10);
+    const distY = parseInt(distance(currentRect, closestRect) - closestRect.height, 10);
+
+    if ((distX >= minDist && distX <= maxDist) || (distY >= minDist && distY <= maxDist)) {
+      currentRect.x = closestRect.x - closestRect.width;
+      currentRect.y = closestRect.y;
+    } */
+
+    console.log(
+      Math.abs(currentRect.x - closestRect.x + closestRect.width) < maxDist,
+      Math.abs(currentRect.x + currentRect.width > closestRect.x) < maxDist
+    );
+
+    // if (Math.abs(currentRect.x - closestRect.x + closestRect.width) < maxDist) {
+    //   currentRect.x = closestRect.x - currentRect.width;
+    // }
+
+    // if (Math.abs(currentRect.x - closestRect.x + closestRect.width) < maxDist) {
+    //   currentRect.x = closestRect.x + closestRect.width;
+    // }
+    // if (Math.abs(activeObject.oCoords.br.y - targ.oCoords.tr.y) < edgedetection) {
+    //   activeObject.top = targ.top - activeObject.currentHeight;
+    // }
+    // if (Math.abs(targ.oCoords.br.y - activeObject.oCoords.tr.y) < edgedetection) {
+    //   activeObject.top = targ.top + targ.currentHeight;
+    // }
+
+    /* const dist = distance(currentRect, closestRect);
+
+    const currentRectLeft = parseInt(dist - currentRect.width, 10);
+    const currentRectRight = parseInt(dist + currentRect.width, 10);
+    const currentRectTop = parseInt(dist - currentRect.height, 10);
+    const currentRectBottom = parseInt(dist + currentRect.height, 10);
+
+    const closestRectLeft = parseInt(dist - closestRect.width, 10);
+    const closestRectRight = parseInt(dist + closestRect.width, 10);
+    const closestRectTop = parseInt(dist - closestRect.height, 10);
+    const closestRectBottom = parseInt(dist + closestRect.height, 10);
+
+    if (Math.abs(currentRectRight - closestRectLeft) < maxDist) {
+      currentRect.x = closestRect.x - currentRect.width;
+    }
+
+    if (Math.abs(currentRectLeft - closestRectRight) < maxDist) {
+      currentRect.x = closestRect.x + closestRect.width;
+    } */
+  }
 }
